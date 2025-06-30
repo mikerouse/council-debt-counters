faa6dd3bc314dddcd15b25e57dafa72ced373920
Mike Rouse <mike@mikerouse.uk>
2025-06-30 10:48:14 +0100

    feat: port WP fields to Laravel models

--- app/Models/Council.php
+++ app/Models/Council.php
@@ -23,7 +23,28 @@ class Council extends Model
         'council_tax_general_grants_income',
         'government_grants_income',
         'all_other_income',
+        'council_type',
+        'council_location',
+        'minimum_revenue_provision',
+        'annual_spending',
+        'annual_deficit',
+        'interest_paid',
+        'capital_financing_requirement',
+        'usable_reserves',
+        'consultancy_spend',
+        'waste_report_count',
+        'statement_of_accounts',
+        'financial_data_source_url',
+        'status_message',
+        'status_message_type',
+        'council_closed',
+        'debt_adjustments',
         'total_debt',
         'total_income',
+    ];
+
+    protected $casts = [
+        'debt_adjustments' => 'array',
+        'council_closed' => 'boolean',
     ];
 }
